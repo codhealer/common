a4e5140abfaad1ff6e6577e91bc37dedad250414
saqqdy <saqqdy@qq.com>
2018-12-21 02:04:00 +0800

    geturlparam

--- common.js
+++ common.js
@@ -636,18 +636,46 @@ function stopBubble(e) {
 }*/
 
 /* 获取URL参数 */
+// function getUrlParam(url) {
+//   var urlStr = url != "" && typeof url != "undefined" ? url.substr(url.indexOf("?")) : location.search; //获取url中"?"符后的字串
+//   var urlParam = [];
+//   if (urlStr.includes("?")) {
+//     var str = urlStr.substr(1);
+//     var strs = str.split("&");
+//     for (var i = 0; i < strs.length; i++) {
+//       //urlParam[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+//       urlParam[strs[i].split("=")[0]] = decodeURI(strs[i].split("=")[1]);
+//     };
+//   };
+//   return urlParam;
+// }
+// function getUrlParam(url) {
+//   // url = (url != "" && typeof url != "undefined") ? url.substr(url.indexOf("?")) : location.search; //获取url中"?"符后的字串
+//   var reg_url = /^[^\?]+\?([\w\W]+)$/,
+//     reg_para = /([^&=]+)=([\w\W]*?)(&|$|#)/g,
+//     arr_url = reg_url.exec(url),
+//     ret = {};
+//   if (arr_url && arr_url[1]) {
+//     var str_para = arr_url[1], result;
+//     while ((result = reg_para.exec(str_para)) != null) {
+//       ret[result[1]] = result[2];
+//     }
+//   }
+//   return ret;
+// }
 function getUrlParam(url) {
-  var urlStr = url != "" && typeof url != "undefined" ? url.substr(url.indexOf("?")) : location.search; //获取url中"?"符后的字串
-  var urlParam = [];
-  if (urlStr.includes("?")) {
-    var str = urlStr.substr(1);
-    var strs = str.split("&");
-    for (var i = 0; i < strs.length; i++) {
-      //urlParam[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
-      urlParam[strs[i].split("=")[0]] = decodeURI(strs[i].split("=")[1]);
-    };
-  };
-  return urlParam;
+  url = (url != "" && typeof url != "undefined") ? url.substr(url.indexOf("?")) : location.href; //获取url中"?"符后的字串
+  var search = url.substring(url.lastIndexOf("?") + 1);
+  var obj = {};
+  var reg = /([^?&=]+)=([^?&=]*)/g;
+  search.replace(reg, function (rs, $1, $2) {
+    var name = decodeURIComponent($1);
+    var val = decodeURIComponent($2);
+    val = String(val);
+    obj[name] = val;
+    return rs;
+  });
+  return obj;
 }
 //console.log(getUrlParam(location.href))
 
